5da967a9e27017ada62a5cb436f0d64de558f47a
ivanreeve <ivanreevelopez@gmail.com>
2025-08-17 02:43:33 +0800

    new defaults

--- gpt_researcher/config/variables/default.py
+++ gpt_researcher/config/variables/default.py
@@ -17,7 +17,7 @@ DEFAULT_CONFIG: BaseConfig = {
     "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
     "MAX_SEARCH_RESULTS_PER_QUERY": 5,
     "MEMORY_BACKEND": "local",
-    "TOTAL_WORDS": 12000,
+    "TOTAL_WORDS": 1200,
     "REPORT_FORMAT": "APA",
     "MAX_ITERATIONS": 3,
     "AGENT_ROLE": None,
